67d49933beb1fcc84be3babea2bd44cdbef949b6
Eli Heuer <elih@protonmail.com>
2024-11-04 19:32:26 -0800

    update kerning groups

--- build-pre-alpha.sh
+++ build-pre-alpha.sh
@@ -2,6 +2,8 @@
 fontmake sources/spoonbender-grotesk-regular-arabic.ufo
 cp master_ttf/spoonbender-grotesk-regular-arabic.ttf fonts/
 python documentation/images/pre-alpha/pre-alpha-001.py --output documentation/images/pre-alpha/pre-alpha-001.png &> /dev/null
+#python documentation/images/pre-alpha/logo.py --output documentation/images/pre-alpha/logo.png &> /dev/null
 #feh --bg-fill documentation/wallpaper/bg-001.png 
 #mtpaint -v documentation/images/pre-alpha/pre-alpha-001.png
 mtpaint documentation/images/pre-alpha/pre-alpha-001.png
+#mtpaint documentation/images/pre-alpha/logo.png
