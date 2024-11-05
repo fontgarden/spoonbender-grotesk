f2307b0309e354b350c27a4de563619fed641397
Eli Heuer <elih@protonmail.com>
2024-11-04 18:19:11 -0800

    update kerning build scripts

--- build-kerning-groups.sh
+++ build-kerning-groups.sh
@@ -5,11 +5,11 @@ lilufo -u sources/spoonbender-grotesk-regular-arabic.ufo \
   --add-kerning-group \
   --group-name "T" \
   --group-side "left" \
-  --group-members "o"
+  --group-members "T"
 
 # Add new right kerning group
 lilufo -u sources/spoonbender-grotesk-regular-arabic.ufo \
   --add-kerning-group \
   --group-name "T" \
   --group-side "right" \
-  --group-members "A"
+  --group-members "T"
